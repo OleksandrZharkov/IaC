baf43c2d8db8f066b7ac1bcefc0bffb7cdd4cee6
OleksandrZharkov <ozharkov.88@gmail.com>
2021-04-30 14:30:15 +0000

    update config

--- main.tf
+++ main.tf
@@ -78,25 +78,6 @@ output "Endpoint" {
   #}
 #}
 
-resource "aws_instance" "linux-instance" {
-  ami           = "ami-0767046d1677be5a0"  #Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
-  instance_type = "t2.micro"
-
-  subnet_id = aws_subnet.32bits-subnet-public-3.id
-
-  vpc_security_group_ids = [aws_security_group.32bits-all.id]
-
-  key_name = "itea"
-
-  tags = {
-    "Name" = "linux-instance"
-  }
-
-  depends_on = [
-    aws_db_instance.32bits-rds,
-  ]
-}
-
 resource "aws_vpc" "32bits-vpc" {
   cidr_block           = "172.31.0.0/16"
   enable_dns_support   = true
@@ -198,3 +179,25 @@ resource "aws_security_group" "32bits-all" {
     "Name" = "32bits-all"
   }
 }
+
+
+resource "aws_instance" "linux-instance" {
+  ami           = "ami-0767046d1677be5a0"  #Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.32bits-subnet-public-3.id
+
+  vpc_security_group_ids = [aws_security_group.32bits-all.id]
+
+  key_name = "itea"
+
+  tags = {
+    "Name" = "linux-instance"
+  }
+
+  depends_on = [
+    aws_db_instance.32bits-rds,
+  ]
+}
+
+
