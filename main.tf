7b5f7b451d74ec6d4ee3af1854aafef1d5d65f5e
OleksandrZharkov <ozharkov.88@gmail.com>
2021-04-30 14:40:01 +0000

    update config

--- main.tf
+++ main.tf
@@ -182,7 +182,7 @@ resource "aws_security_group" "x3iibits-all" {
 
 
 resource "aws_instance" "linux-instance" {
-  ami           = "ami-0767046d1677be5a0"  #Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+  ami           = "009b16df9fcaac611"  
   instance_type = "t2.micro"
 
   subnet_id = aws_subnet.x3iibits-subnet-public-3.id
