7282f8cb0e0fd0b73e7d60ac76a2e46b221fd32b
OleksandrZharkov <ozharkov.88@gmail.com>
2021-04-30 14:53:02 +0000

    update config

--- main.tf
+++ main.tf
@@ -182,7 +182,7 @@ resource "aws_security_group" "x3iibits-all" {
 
 
 resource "aws_instance" "linux-instance" {
-  ami           = "009b16df9fcaac611"  
+  ami           = "ami-009b16df9fcaac611"  
   instance_type = "t2.micro"
 
   subnet_id = aws_subnet.x3iibits-subnet-public-3.id
